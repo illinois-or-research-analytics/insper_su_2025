7a2f15b728e31757a79b0152d750e2d4b9961249
Esther Caroline Cunha Rodrigues <estherccr@al.insper.edu.br>
2025-06-11 11:50:26 -0500

    trying with bigger samples and perfecting the code

--- ecc7/scripts/sampling.py
+++ ecc7/scripts/sampling.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import numpy as np
 import time
+import click 
 import seaborn as sns
 import matplotlib.pyplot as plt
 import xml.etree.ElementTree as ET
@@ -46,7 +47,27 @@ def get_references(xml_data):
             count += 1
     return count
 
-def build_sample_from_range(id_2020, id_2025, sample_size=10, output_dir="../outputs", filename="pubmed_references.csv"):
+
+@click.command(
+        help='''   Generates a sample of PubMed journal articles with at least 5 references,
+    selected randomly between a given range of PMIDs
+
+    Parameters:
+        id_2020 (str): PMID from year 2020
+        id_2025 (str): PMID from year 2025
+        sample_size (int): Number of valid samples to collect
+        output_dir (str): Directory to store the CSV output
+        filename (str): Name of the output CSV file without extension
+
+    Returns:
+        pd.DataFrame: DataFrame containing sampled PMIDs and their out-degree (reference count)'''
+)
+@click.option('--id_2020', type=str, required=True, help='PMID from year 2020')
+@click.option('--id_2025', type=str, required=True, help='PMID from year 2025')
+@click.option('--sample_size', type=int, required=True, help='Number of valid samples to collect')
+@click.option('--output_dir', type=str, required=False, default='../outputs', help='Directory to store the CSV output')
+@click.option('--filename', type=str, required=False, default='pubmed_references.csv', help='Name of the output CSV file (without extension)')
+def build_sample_from_range(id_2020, id_2025, sample_size, output_dir, filename):
     """
     Generates a sample of PubMed journal articles with at least 5 references,
     selected randomly between a given range of PMIDs
@@ -56,7 +77,7 @@ def build_sample_from_range(id_2020, id_2025, sample_size=10, output_dir="../out
         id_2025 (str): PMID from year 2025
         sample_size (int): Number of valid samples to collect
         output_dir (str): Directory to store the CSV output
-        filename (str): Name of the output CSV file
+        filename (str): Name of the output CSV file without extension
 
     Returns:
         pd.DataFrame: DataFrame containing sampled PMIDs and their out-degree (reference count)
@@ -66,6 +87,7 @@ def build_sample_from_range(id_2020, id_2025, sample_size=10, output_dir="../out
 
     # Ensure output directory exists
     os.makedirs(output_dir, exist_ok=True)
+    filename = filename + f'_{sample_size}.csv'
     output_path = os.path.join(output_dir, filename)
 
     # Sample articles until the desired number of valid articles is collected
@@ -81,7 +103,7 @@ def build_sample_from_range(id_2020, id_2025, sample_size=10, output_dir="../out
                 reference_pmids = get_references(xml_data)
 
                 # Accept only if the article has at least 5 references
-                if reference_pmids >= 5 and is_journal_article(xml_data):
+                if reference_pmids >= 5 and is_journal_article(xml_data) and pmid not in valid_pmids:
                     valid_pmids.append(pmid)
                     refs.append(reference_pmids)
                     break
@@ -101,11 +123,9 @@ def build_sample_from_range(id_2020, id_2025, sample_size=10, output_dir="../out
 
     return df
 
-# Example run: generate a sample of 10,000 articles
-df = build_sample_from_range(
-    id_2020='31901868',
-    id_2025='40486797',
-    sample_size=10000,
-    output_dir="../outputs",
-    filename="pubmed_references_10k.csv"
-)
+
+if __name__ == "__main__":
+    build_sample_from_range()
+
+# id_2020 = 31901868
+# id_2025 = sa
